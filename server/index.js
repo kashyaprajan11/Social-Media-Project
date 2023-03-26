200a41a6007d8603575b74a24f706953352c7c44
kashyaprajan11 <rkashyap251@gmail.com>
2023-03-26 16:46:38 +0530

    Connected with mongodb database

--- server/index.js
+++ server/index.js
@@ -2,3 +2,18 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://rajan:RajanKashyap@atlascluster.2v0dett.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() => app.listen(PORT, () => console.log("Connected with database")))
+  .catch(() => console.log("Some problem encountered, cannot connect"));
